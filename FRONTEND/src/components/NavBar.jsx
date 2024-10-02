e48195483cac9eba8180901311b71f1355df3592
nagesh-merva <officialnagesh.merva@gmail.com>
2024-10-02 15:27:26 +0530

    changes

--- FRONTEND/src/components/NavBar.jsx
+++ FRONTEND/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { FaBars } from "react-icons/fa6"
+// import { FaBars } from "react-icons/fa6"
 
 
 function NavBar() {
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <div className="h-16 w-full z-50 bg-gray-950/90 fixed flex items-center top-0">
             <img src="/ecell.png" alt="ecelllogo" className="h-14 px-2 py-1 w-auto" />
-            <FaBars className="absolute right-6 text-white text-xl" />
+            {/* <FaBars className="absolute right-6 text-white text-xl" /> */}
         </div>
     )
 }
