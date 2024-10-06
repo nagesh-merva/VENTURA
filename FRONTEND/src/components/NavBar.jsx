2371de42ff755abd519af38a1b20d610a71be8a9
nagesh-merva <officialnagesh.merva@gmail.com>
2024-10-06 23:23:51 +0530

    logo change

--- FRONTEND/src/components/NavBar.jsx
+++ FRONTEND/src/components/NavBar.jsx
@@ -21,7 +21,7 @@ function NavBar() {
 
     return (
         <div className="h-16 w-full z-50 bg-gray-950/90 fixed flex items-center top-0 left-0">
-            <img src="/ecell.png" alt="ecelllogo" className="h-14 px-2 py-1 w-auto" />
+            <img src="/ecell.jpg" alt="ecelllogo" className="h-14 px-2 py-1 w-auto" />
             <FaBars
                 className="absolute right-6 text-white text-xl cursor-pointer"
                 onClick={handleDropdownClick}
